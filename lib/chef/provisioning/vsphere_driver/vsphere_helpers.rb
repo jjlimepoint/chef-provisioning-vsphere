18bfeea93ab51120e27d14532d77dda6e5502361
Jaymz Julian <jjulian@limepoint.com>
2020-07-04 06:37:22 +1000

    make that shutdown timeout 600

--- lib/chef/provisioning/vsphere_driver/vsphere_helpers.rb
+++ lib/chef/provisioning/vsphere_driver/vsphere_helpers.rb
@@ -95,7 +95,7 @@ module ChefProvisioningVsphere
         if shutdown_retry > 0
           puts "WARNING: vsphere threw #{e.to_s} when initiating shutdown.... retrying"
           shutdown_retry -= 1
-          sleep(60)
+          sleep(600)
           retry
         end
         vm.PowerOffVM_Task.wait_for_completion
